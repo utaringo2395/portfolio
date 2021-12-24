246ef18b95cf8583caecf7cc4c76e462d6e332bd
Mariana Macias <mariana.macias2395@gmail.com>
2021-12-24 13:31:32 -0600

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../styles/Home.module.css";
 import stylesComponent from "../styles/Component.module.css";
+import stylesNav from "../styles/Nav.module.css";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -14,74 +16,24 @@ export default function Home() {
         <link rel="icon" href="/mmm.ico" />
       </Head>
       <header className="msticky top-0 z-50">
-        <nav
-          className="
-              my-4
-              flex flex-wrap
-              items-center
-              justify-between
-              w-full
-              py-4
-              
-              md:py-0
-              text-lg text-gray-700
-              
-            "
-        >
-          <div>
-            <Link href="/" className={styles.brand}>
-              mmmacias
-            </Link>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            id="menu-button"
-            className="h-6 w-6 cursor-pointer md:hidden block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-
-          <div
-            className="hidden w-full md:flex md:flex-col md:items-center md:w-auto"
-            id="menu"
-          >
-            <ul
-              className="
-                  pt-4
-                  text-base text-black
-                  md:flex
-                  md:justify-between 
-                  md:pt-0"
-            >
-              <li className={styles.hoverAnimation}>
-                <Link
-                  className="md:p-4 py-2 block"
-                  href="/about"
-                  rel="noopener noreferrer"
-                >
-                  about me
-                </Link>
-              </li>
-              <li className={styles.hoverAnimation}>
-                <Link
-                  className="md:p-4 py-2 block"
-                  href="/contact"
-                  rel="noopener noreferrer"
-                >
-                  contact
-                </Link>
-              </li>
-            </ul>
-          </div>
+        <nav className={stylesNav.navigation}>
+          <div className={stylesNav.logo}>mmmacias</div>
+          <FontAwesomeIcon id="click" icon="check-square" />
+          <label htmlFor="click" className="menu-btn">
+            <i className="fas fa-bars"></i>
+          </label>
+          <ul className={stylesNav.navList}>
+            <li className={stylesNav.navItem}>
+              <Link className={stylesNav.navLink} href="/about">
+                about
+              </Link>
+            </li>
+            <li className={stylesNav.navItem}>
+              <Link className={stylesNav.navLink} href="/contact">
+                contact
+              </Link>
+            </li>
+          </ul>
         </nav>
       </header>
 
